96a112cc639d5d6fb9985b83622870a377b5d11c
turinglabsorg <turinglabs@criptext.com>
2019-10-28 16:40:59 +0100

    install script fix

--- connect/install.sh
+++ connect/install.sh
@@ -39,3 +39,20 @@ logtimestamps=1" > "/root/.lyra/lyra.conf"
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm install pm2 -g
+
+#SETTING UP FIREWALL
+ufw allow 22
+ufw deny 42223
+ufw deny 27017
+ufw allow 42222
+ufw enable y
+
+#SETTING UP NGINX
+sudo apt update
+sudo apt install nginx -y
+sudo ufw allow 'Nginx Full'
+
+#INSTALL CERTBOT
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt update
+sudo apt install python-certbot-nginx -y
